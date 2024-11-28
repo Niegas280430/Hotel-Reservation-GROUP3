5db5e7e5cad31980323e63929e942341432ea5cc
Niegas <gianiegas@gmail.com>
2024-11-28 08:22:11 +0800

    add testDatabase

--- TestDatabase/TestDatabase/MainMenuForm.Designer.cs
+++ TestDatabase/TestDatabase/MainMenuForm.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(224, 203);
+            this.button1.Location = new System.Drawing.Point(220, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(366, 141);
             this.button1.TabIndex = 0;
