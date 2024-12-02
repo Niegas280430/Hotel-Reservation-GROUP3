f70504482be658caa086c3f8e654dd37b1d9ab99
paulbamo7 <paulbamo7@gmail.com>
2024-12-02 15:26:42 +0800

    Add Sql Reference

---
+++ WindowForms/WindowForms/ConnectDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowForms
+{
+    internal class ConnectDatabase
+    {
+        public SqlConnection Connect()
+        {
+            SqlConnection connect = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=HotelManagementSystem;Integrated Security=True");
+            return connect;
+        }
+    }
+}
