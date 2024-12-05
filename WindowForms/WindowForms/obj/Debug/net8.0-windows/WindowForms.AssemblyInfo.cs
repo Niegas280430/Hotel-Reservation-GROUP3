753e8dd23bd6755ac1864512ab45a220d8108a83
Niegas <niegas.280430@ortigas-cainta.sti.edu.ph>
2024-12-05 20:39:46 +0800

    Add two new form
    - AvailabilityForm
    - BookingConfirmation

--- WindowForms/WindowForms/obj/Debug/net8.0-windows/WindowForms.AssemblyInfo.cs
+++ WindowForms/WindowForms/obj/Debug/net8.0-windows/WindowForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+733282209708a92fe2cac8f84989909ca6c3a508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55e8a168f40e6c3e1af71e4ec80778361120cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
