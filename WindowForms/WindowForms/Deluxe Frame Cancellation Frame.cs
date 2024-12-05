11029074b27e0aeeaaf0e912b047e8f6b9694de2
Niegas <niegas.280430@ortigas-cainta.sti.edu.ph>
2024-12-05 17:20:37 +0800

    add ten uc rooms frames

---
+++ WindowForms/WindowForms/Deluxe Frame Cancellation Frame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowForms
+{
+    public partial class Deluxe_Frame_Cancellation_Frame : UserControl
+    {
+        public Deluxe_Frame_Cancellation_Frame()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WindowForms/WindowForms/obj/Release/net8.0-windows/WindowForms.AssemblyInfo.cs
+++ WindowForms/WindowForms/obj/Release/net8.0-windows/WindowForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bc10dc00cae4060d6677752d52087f92a2d96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34a66ff1119db8137c4bfbd68aa2faf9a361d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
