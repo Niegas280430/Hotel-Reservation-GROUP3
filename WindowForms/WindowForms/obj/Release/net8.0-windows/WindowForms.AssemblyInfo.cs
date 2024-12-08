3a35d70e4db16260e0e6b866857b8b1fec2f1a06
paulbamo7 <paulbamo7@gmail.com>
2024-12-09 02:24:07 +0800

    Manage Frames and add database

--- WindowForms/WindowForms/obj/Release/net8.0-windows/WindowForms.AssemblyInfo.cs
+++ WindowForms/WindowForms/obj/Release/net8.0-windows/WindowForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278ccc328a738961c7257f761bed3a4975dce7cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10092429f7b6227982d17f475cdb88a4fdcbcbf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
